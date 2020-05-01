a061d916b8134b9163edc6e745d60fc47ba4cc20
Felicity Ratcliffe <felicity.ratcliffe@moneysupermarket.com>
2020-05-01 14:59:38 +0100

    fix example

--- src/main/java/uk/fratcliffe/fl2j/WorldController.java
+++ src/main/java/uk/fratcliffe/fl2j/WorldController.java
@@ -4,13 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Controller;
+
 import uk.fratcliffe.fl2j.models.Embedded;
 
+@Controller
 @RestController
 public class WorldController {
     @Autowired
     private TeleportClient teleportClient;
     @RequestMapping("/Cities")
+    @ResponseBody
     public Embedded city(@RequestParam String name) {
         Embedded city = teleportClient.getCity(name);
         return city;
---
+++ src/main/java/uk/fratcliffe/fl2j/config/TomcatConfiguration.java
@@ -0,0 +1,16 @@
+package uk.fratcliffe.fl2j.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfiguration {
+    private Connector redirectConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
+}
